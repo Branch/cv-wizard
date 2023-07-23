7bbdb953767f435832f9f3a8908466c07e9f0bd0
Andreas Beuger <andreasbeuger@gmail.com>
2023-07-23 14:51:23 +0200

    feat: basic page layout implementation, header and footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import '../styles/globals.css'
+import Link from 'next/link'
+import Nav from '@/components/ui/native/nav'
 
 const inter = Inter({
     subsets: ['latin'],
@@ -15,8 +17,39 @@ export default function RootLayout({
     children: React.ReactNode
 }) {
     return (
-        <html lang="en" className={inter.className}>
-            <body>{children}</body>
+        <html lang="sv" className={inter.className}>
+            <body>
+                <div className="fixed bg-white/90 backdrop-blur-sm h-16 z-10 flex justify-between w-full px-24 border-b items-center">
+                    <div className="flex gap-4 justify-center items-center">
+                        <Link href={'/'}>Min logga</Link>
+                        <Nav />
+                    </div>
+                </div>
+                {children}
+                <footer className="container text-sm py-12">
+                    <div className="border-b pb-12 mb-12">
+                        <h3 className="font-bold">Kom igång</h3>
+                        <ul className="flex flex-col gap-2 mt-2 text-slate-400">
+                            <li>
+                                <Link className=" hover:text-primary" href={''}>
+                                    Välj mall
+                                </Link>
+                            </li>
+                            <li>
+                                <Link className=" hover:text-primary" href={''}>
+                                    Fyll i dina uppgifter
+                                </Link>
+                            </li>
+                            <li>
+                                <Link className=" hover:text-primary" href={''}>
+                                    Exportera
+                                </Link>
+                            </li>
+                        </ul>
+                    </div>
+                    <Link href={'/'}>Min logga</Link>
+                </footer>
+            </body>
         </html>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,23 +3,33 @@ import { Button } from '@/components/ui/button'
 
 export default function Home() {
     return (
-        <main className="flex flex-col items-center justify-between py-24">
-            <section className="container mx-auto flex flex-col justify-center items-center px-24">
-                <h1 className="text-6xl block font-extrabold leading-tight text-center">
+        <main className="flex flex-col items-center justify-between py-24 relative overflow-hidden">
+            <div className="bg-[url('/img/hero.svg')] w-full h-full absolute bg-no-repeat scale-[3] bg-center z-0"></div>
+            <header className="container mx-auto flex flex-col justify-center items-center pt-24 px-24 relative">
+                <h1 className="text-6xl block font-extrabold text-center">
                     Bygg ditt CV snabbt och enkelt, helt gratis.
                 </h1>
                 <p className="py-8 px-52 text-center">
                     Inga jobbiga konton inblandade. Välj bland populära mallar
                     såsom{' '}
-                    <Link className="underline" href={'/'}>
+                    <Link
+                        className="text-highlightPrimary font-bold"
+                        href={'/'}
+                    >
                         Zesam
                     </Link>
                     ,{' '}
-                    <Link className="underline" href={'/'}>
+                    <Link
+                        className="text-highlightPrimary font-bold"
+                        href={'/'}
+                    >
                         Clean
                     </Link>{' '}
                     eller{' '}
-                    <Link className="underline" href={'/'}>
+                    <Link
+                        className="text-highlightPrimary font-bold"
+                        href={'/'}
+                    >
                         1337
                     </Link>{' '}
                     och kom igång direkt.
@@ -30,7 +40,7 @@ export default function Home() {
                     </Button>
                     <Button variant={'secondary'}>Läs mer</Button>
                 </div>
-            </section>
+            </header>
         </main>
     )
 }
